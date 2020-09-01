fd4fc5fb29516163d16491ed3406729a895c8c4f
khaifahmi99 <khaifahmi99@github.com>
2020-09-01 12:15:50 +1000

    implement the Grid Gallery Component

--- src/components/SingleGalleryPage.js
+++ src/components/SingleGalleryPage.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Gallery from 'react-grid-gallery';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -11,21 +12,36 @@ function SingleGalleryPage(props) {
 
     const [loading, setLoading] = useState(true);
     const [gallery, setGallery] = useState({});
+    const [images, setImages] = useState([]);
+
+    const galleryObj = useRef(null);
 
     useEffect(() => {
+        async function formatImages(gallery) {
+            gallery.urls.forEach((url) => {
+                const img = {
+                    src: url,
+                    thumbnail: url,
+                    thumbnailWidth: 320,
+                    thumbnailHeight: 212,
+                }
+                setImages(images => [...images, img])
+            })
+        }
+
         async function getData() {
             try {
                 await db.collection('gallery').doc(id)
                     .get()
                     .then((doc) => {
                         setGallery(doc.data());
+                        formatImages(doc.data());
                         setLoading(false);
                     })
             } catch (e) {
                 console.log('Single Gallery Page: Error in fetching gallery in db');
             }
         }
-
         getData();
     }, [])
 
@@ -63,9 +79,20 @@ function SingleGalleryPage(props) {
                     <p><i class="fa fa-calendar-o" aria-hidden="true"></i> {convertSecondsToDate(gallery.ts.seconds)}</p>
                     <p> <i class="fa fa-map-marker" aria-hidden="true"></i> {gallery.location}</p>
 
-                    
+                    <div className="white" style={{
+                    display: "block",
+                    minHeight: "1px",
+                    width: "100%",
+                    border: "1px solid #ddd",
+                    overflow: "auto"}}>
+                        <Gallery images={images} ref={galleryObj} showLightboxThumbnails={true}/>
+                    </div>
+                    <br/>
+                    <div className="center-align">
+                        <a href="/galleries" className="btn black white-text">Return to Galleries</a>
+                    </div>
+                    <br/><br/>
                 </div>
-    
                 <Footer />
             </div>
         );
