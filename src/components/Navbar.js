b26667582832f1e02d2beeb73132e0b0b8487804
khaifahmi99 <khaifahmi99@github.com>
2021-08-08 19:53:38 +1000

    make sidebar clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,72 +11,79 @@ export default class Navbar extends React.Component {
 
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="black">
-          <div className="nav-wrapper">
-            <div className="row">
-              <div className="col s4">
-                <a
-                  href="/"
-                  className="brand-logo hide-on-med-and-down"
-                  style={{ paddingLeft: '24px', fontFamily: 'comic sans ms' }}
+      <>
+        <div className="navbar-fixed">
+          <nav className="black">
+            <div className="nav-wrapper">
+              <div className="row">
+                <div className="col s4">
+                  <a
+                    href="/"
+                    className="brand-logo hide-on-med-and-down"
+                    style={{ paddingLeft: '24px', fontFamily: 'comic sans ms' }}
+                  >
+                    KHAI FAHMI.
+                  </a>
+                  <a
+                    href="/"
+                    className="brand-logo center-align hide-on-large-only"
+                    style={{ fontFamily: 'comic sans ms' }}
+                  >
+                    KHAI FAHMI.
+                  </a>
+                  <button
+                    style={{
+                      backgroundColor: 'black',
+                      color: 'white',
+                      border: 'none',
+                    }}
+                    data-target="mobile-demo"
+                    className="sidenav-trigger"
+                  >
+                    <i className="material-icons">menu</i>
+                  </button>
+                </div>
+                <ul
+                  className="right hide-on-med-and-down"
+                  style={{ fontFamily: 'Montserrat' }}
                 >
-                  KHAI FAHMI.
-                </a>
-                <a
-                  href="/"
-                  className="brand-logo center-align hide-on-large-only"
-                  style={{ fontFamily: 'comic sans ms' }}
-                >
-                  KHAI FAHMI.
-                </a>
-                {/* <a
-                  data-target="mobile-demo"
-                  className="sidenav-trigger"
-                >
-                  <i className="material-icons">menu</i>
-                </a> */}
+                  <li>
+                    <a href="/projects">Projects</a>
+                  </li>
+                  {shouldSeeAcademics() && (
+                    <li>
+                      <a href="/academics">Academics</a>
+                    </li>
+                  )}
+                  <li>
+                    <a href="/contact">Contact</a>
+                  </li>
+                </ul>
               </div>
-              <ul
-                className="right hide-on-med-and-down"
-                style={{ fontFamily: 'Montserrat' }}
-              >
-                <li>
-                  <a href="/projects">Projects</a>
-                </li>
-                {shouldSeeAcademics() && (
-                  <li>
-                    <a href="/academics">Academics</a>
-                  </li>
-                )}
-                <li>
-                  <a href="/contact">Contact</a>
-                </li>
-              </ul>
-              <ul
-                className="sidenav"
-                id="mobile-demo"
-                ref={(sidenav) => {
-                  this.sidenav = sidenav;
-                }}
-                style={{ fontFamily: 'Montserrat' }}
-              >
-                <li>
-                  <a href="/projects">Projects</a>
-                </li>
-                {shouldSeeAcademics() && (
-                  <li>
-                    <a href="/academics">Academics</a>
-                  </li>
-                )}
-                <li>
-                  <a href="/contact">Contact</a>
-                </li>
-              </ul>
             </div>
-          </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
+        <ul
+          className="sidenav"
+          id="mobile-demo"
+          ref={(sidenav) => {
+            this.sidenav = sidenav;
+          }}
+          style={{ fontFamily: 'Montserrat' }}
+        >
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          {shouldSeeAcademics() && (
+            <li>
+              <a href="/academics">Academics</a>
+            </li>
+          )}
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </>
     );
   }
 }
